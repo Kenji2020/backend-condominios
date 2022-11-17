2ce07aa8d900e11e904fddec38381cd7cb92b73e
Kenji2020 <83725041+Kenji2020@users.noreply.github.com>
2022-11-17 13:39:12 -0300

    Update index.js

--- index.js
+++ index.js
@@ -8,4 +8,4 @@ app.use(cors())
 app.use(express.json())
 app.use(userroutes)
 
-app.listen(3000, () => console.log('Server running on port 3000'))
+app.listen(5000, () => console.log('Server running on port 5000'))
