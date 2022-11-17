555975dbeb6514347aa883806ab1c525251da5c2
Kenji2020 <83725041+Kenji2020@users.noreply.github.com>
2022-11-17 16:31:49 -0300

    Update userroutes.js

--- routes/userroutes.js
+++ routes/userroutes.js
@@ -1,6 +1,7 @@
 import { Router } from "express"
 
 import { getUser, getUsers, deleteUser, editUser, createUser } from "../controllers/controllers.js"
+import { pool } from "../Components/db.js";
 
 const router = Router()
 router.get('/', (req, res) => {
@@ -8,7 +9,13 @@ router.get('/', (req, res) => {
     })
 
 router.get('/users', (req, res) => {
-    res.send('Hello World!')
+    try {
+        const promisePool = pool.promise();
+        const [result] = promisePool.query("select  * from users ");
+        res.json(result);
+      } catch (error) {
+        return res.status(500).json({ message: error.message });
+      }
     })
 router.get('/users/:id', getUser)
 router.delete('/users/:id', deleteUser)
