e73890d98820d31c7d580eff04e21cdd2b2a1ed9
Kenji2020 <83725041+Kenji2020@users.noreply.github.com>
2022-11-17 13:38:10 -0300

    Update userroutes.js

--- routes/userroutes.js
+++ routes/userroutes.js
@@ -3,7 +3,10 @@ import { Router } from "express"
 import { getUser, getUsers, deleteUser, editUser, createUser } from "../controllers/controllers.js"
 
 const router = Router()
-
+router.get('/', (req, res) => {
+    res.send('Hello World!')
+    })
+    
 router.get('/users', getUsers)
 router.get('/users/:id', getUser)
 router.delete('/users/:id', deleteUser)
