c0e2ae3103d7f4da43340f0c8b9dfd9064ca6514
Kenji2020 <83725041+Kenji2020@users.noreply.github.com>
2022-11-17 21:21:06 -0300

    only

---
+++ Components/db.js
@@ -0,0 +1,12 @@
+import { createPool } from 'mysql2'
+import * as dotenv from 'dotenv'
+dotenv.config()
+import express from 'express'
+export const pool = createPool({
+        host: 'condominios.cifm8lfferqz.us-west-2.rds.amazonaws.com',
+        user: 'victorinfluencia',
+        password: 'colocolo5631',
+        database: 'condominios',
+        port: '5463',
+})
+
