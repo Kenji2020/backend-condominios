82d7ce82b3db88644abc00943c1dffe63dcd2a97
Kenji2020 <83725041+Kenji2020@users.noreply.github.com>
2022-11-20 12:56:49 -0300

    b

--- index.js
+++ index.js
@@ -1,36 +1,11 @@
-const app = require("express")();
-const server = require("http").createServer(app);
-const cors = require("cors");
+import express from 'express'
+const app = express()
+import cors from "cors"
+import userroutes from "./routes/userroutes.js"
 
-const io = require("socket.io")(server, {
-	cors: {
-		origin: "*",
-		methods: [ "GET", "POST" ]
-	}
-});
 
-app.use(cors());
+app.use(cors())
+app.use(express.json())
+app.use(userroutes)
 
-const PORT = process.env.PORT || 5001;
-
-app.get('/', (req, res) => {
-	res.send('Running');
-});
-
-io.on("connection", (socket) => {
-	socket.emit("me", socket.id);
-
-	socket.on("disconnect", () => {
-		socket.broadcast.emit("callEnded")
-	});
-
-	socket.on("callUser", ({ userToCall, signalData, from, name }) => {
-		io.to(userToCall).emit("callUser", { signal: signalData, from, name });
-	});
-
-	socket.on("answerCall", (data) => {
-		io.to(data.to).emit("callAccepted", data.signal)
-	});
-});
-
-server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+app.listen(3000, () => console.log('Server running on port 3000'))
